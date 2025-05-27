9f78e4d0d0c58d5c87b83ffb85f0cfae255613f2
Lina Kent <linak9@nycstudents.net>
2025-05-27 10:46:04 -0400

    optimizations

--- code0.js
+++ code0.js
@@ -399,7 +399,7 @@ for(var i = 0, len = gdjs.OverworldCode.GDTilemap_9595WaterObjects1.length ;i < 
     gdjs.OverworldCode.GDDialogBoxObjects1[i].hide();
 }
 }{for(var i = 0, len = gdjs.OverworldCode.GDBush1Objects1.length ;i < len;++i) {
-    gdjs.OverworldCode.GDBush1Objects1[i].getBehavior("Scale").setScale(0.5);
+    gdjs.OverworldCode.GDBush1Objects1[i].getBehavior("Scale").setScale(1.0);
 }
 }}
 
